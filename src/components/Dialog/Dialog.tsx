668e8f92b3f458fcc1d647e9b440909c49b6be46
suny-saja <muhsinsuny@yahoo.com>
2025-02-16 17:09:00 +0700

    Update Dialog.tsx

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,76 +1,122 @@
 import React from 'react';
-import Button from '../Buttons/Button';
-import styles from './Dialog.module.scss';
+import Button from '../Button/Button';
+import style from './Dialog.module.scss';
 
 type DialogProps = {
+  disabled?: boolean;
   imgSrc?: string;
-  type: 'info' | 'danger' | 'success';
-  isOpen: boolean;
+  variant?:
+    | 'success'
+    | 'info'
+    | 'danger'
+    | 'disabledInfo'
+    | 'disabledDanger'
+    | null;
+  isOpen?: boolean;
   onClose: () => void;
-  onConfirm?: () => void;
+  onConfirm: () => void;
 };
 
-const DialogBox: React.FC<DialogProps> = ({
-  type,
+export const Dialog: React.FC<DialogProps> = ({
+  disabled = false,
+  variant = 'success',
   isOpen,
   onClose,
   onConfirm,
 }) => {
   if (!isOpen) return null;
 
-  const title = {
-    info: 'New Information',
-    danger: 'Are you sure you want to delete this user?',
-    success: 'Successfully loaded',
-  }[type];
+  const imgSrc = variant
+    ? {
+        success: './src/assets/logo-success.svg',
+        info: './src/assets/logo-info.svg',
+        danger: './src/assets/logo-danger.svg',
+        disabledInfo: './src/assets/logo-info.svg',
+        disabledDanger: './src/assets/logo-danger.svg',
+      }[variant]
+    : '';
 
-  const imgSrc = {
-    info: './../../infoLogo.svg',
-    danger: './../../dangerLogo.svg',
-    success: './../../successLogo.svg',
-  }[type];
+  const title = variant
+    ? {
+        success: 'Successfully loaded',
+        info: 'New information',
+        danger: 'Are you sure you want to delete this user?',
+        disabledInfo: 'New information',
+        disabledDanger: 'Are you sure you want to delete this user?',
+      }[variant]
+    : '';
 
-  const description = {
-    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    danger: 'This action is irreversible',
-    success: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  }[type];
+  const description = variant
+    ? {
+        success: 'lorem ipsum dolor sit amet',
+        info: 'lorem ipsum dolor sit amet',
+        danger: 'This action is irreversible',
+        disabledInfo: 'lorem ipsum dolor sit amet',
+        disabledDanger: 'This action is irreversible',
+      }[variant]
+    : '';
 
-  const buttons = {
-    success: [{ label: 'OK', variant: 'secondary', action: onClose }],
-    info: [
-      { label: 'Cancel', variant: 'secondary', action: onClose },
-      { label: 'Create', variant: 'primary', action: onConfirm },
-    ],
-    danger: [
-      { label: 'Cancel', variant: 'secondary', action: onClose },
-      { label: 'Delete', variant: 'danger', action: onConfirm },
-    ],
-  }[type];
+  const buttons = variant
+    ? {
+        success: [{ label: 'OK', type: 'secondary', action: onClose }],
+        info: [
+          { label: 'Cancel', type: 'secondary', action: onClose },
+          { label: 'Create', type: 'primary', action: onConfirm },
+        ],
+        danger: [
+          { label: 'Cancel', type: 'secondary', action: onClose },
+          { label: 'Delete', type: 'danger', action: onConfirm },
+        ],
+        disabledInfo: [
+          { label: 'Cancel', type: 'secondary', action: false },
+          { label: 'Create', type: 'primary', action: false },
+        ],
+        disabledDanger: [
+          { label: 'Cancel', type: 'secondary', action: false },
+          { label: 'Delete', type: 'danger', action: false },
+        ],
+      }[variant]
+    : '';
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.dialog}>
-        <img src={imgSrc} alt='logo' className={styles.img} />
-        <div className={styles.dialogContent}>
-          <h2 className={styles.title}>{title}</h2>
-          <p className={styles.description}>{description}</p>
+    <>
+      <div className={style.dialogBox}>
+        <div className={style.container}>
+          <div className={style.wrapper}>
+            <img className={style.logo} src={imgSrc} alt='logo' />
+            <div className={style.content}>
+              <h2 className={style.title}>{title}</h2>
+              <p className={style.description}>{description}</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.footer}>
+          <div className={style.buttons}>
+            {Array.isArray(buttons) &&
+              buttons.map((button, index) => (
+                <Button
+                  disabled={disabled}
+                  key={index}
+                  label={button.label}
+                  type={
+                    button.type as
+                      | 'primary'
+                      | 'secondary'
+                      | 'danger'
+                      | 'outlined'
+                  }
+                  onClick={
+                    typeof button.action === 'function'
+                      ? button.action
+                      : undefined
+                  }
+                />
+              ))}
+          </div>
         </div>
       </div>
-      <div className={styles.overlay}>
-        <div className={styles.dialog__buttons}>
-          {buttons.map((btn, index) => (
-            <Button
-              key={index}
-              label={btn.label}
-              variant={btn.variant as 'secondary' | 'primary' | 'danger'}
-              onClick={btn.action || (() => {})}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
-export default DialogBox;
+export default Dialog;
